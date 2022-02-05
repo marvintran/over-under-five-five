8dd18c9b4dad485a4acdf7bd4704ccfc966ea4d1
marvintran <marvintran1234@gmail.com>
2022-02-04 23:24:01 -0600

    Update incorrect PORT environment variable

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const ENV = process.env.DB_ENVIRONMENT || "development";
-const PORT = process.env.DB_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const pool = require("./db");
 const path = require('path');
 
