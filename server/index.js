de4f7f4728d9da7c4898d323123cfbbbb4cc725a
marvintran <marvintran1234@gmail.com>
2022-02-10 21:26:14 -0600

    Remove using build files

--- server/index.js
+++ server/index.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const ENV = process.env.DB_ENVIRONMENT || "development";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.REACT_APP_DB_PORT;
 const pool = require("./db");
 const path = require('path');
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-if (ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.use((req,res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
 
 // ROUTES
 
