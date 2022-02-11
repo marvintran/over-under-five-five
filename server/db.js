96a0499b3f56a8420490460033cf9f8d53821fdd
marvintran <marvintran1234@gmail.com>
2022-02-10 21:24:24 -0600

    Change logic of prodution Pool to use environment variable

--- server/db.js
+++ server/db.js
@@ -1,15 +1,6 @@
 const Pool = require("pg").Pool;
 require('dotenv').config();
-const database = process.env.DB_ENVIRONMENT || "development";
-
-const poolProd = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  ssl: true
-});
+const database = process.env.NODE_ENV || "development";
 
 const poolDev = new Pool({
   user: process.env.REACT_APP_DB_USER,
@@ -17,6 +8,11 @@ const poolDev = new Pool({
   host: process.env.REACT_APP_DB_HOST,
   port: process.env.REACT_APP_DB_PORT,
   database: process.env.REACT_APP_DB_DATABASE
+});
+
+const poolProd = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 if(database === "development") {
