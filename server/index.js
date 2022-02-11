a2a2a4308a4d6ddb8de8804c14dfafb7c687a0c3
marvintran <marvintran1234@gmail.com>
2022-02-10 20:39:05 -0600

    Require path

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const ENV = process.env.DB_ENVIRONMENT || "development";
-const PORT = process.env.DB_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const pool = require("./db");
+const path = require('path');
 
 // middleware
 app.use(cors());
